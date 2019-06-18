ee9397d710e56f4d5cd00d32f088a78be35dc32d
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-18 11:49:16 -0700

    Sign-up route working

--- src/main/java/com/fernj/lab401/codefellowship/FellowshipController.java
+++ src/main/java/com/fernj/lab401/codefellowship/FellowshipController.java
@@ -8,10 +8,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -40,15 +42,24 @@ public class FellowshipController {
     }
 
     @PostMapping("/signup")
-    public RedirectView createUser(String username, String password, String firstName, String lastName, Date birthDate, String bio) {
+    public String createUser(@ModelAttribute ApplicationUser appUser) {
 
-        ApplicationUser newUser = new ApplicationUser(username, passwordEncoder.encode(password), firstName,
-                lastName, birthDate, bio);
+        ApplicationUser newUser = new ApplicationUser(appUser.username, passwordEncoder.encode(appUser.password),
+                appUser.firstName, appUser.lastName, appUser.dateOfBirth, appUser.bio);
 
         appUserRepository.save(newUser);
-        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
+        ApplicationUser user = appUserRepository.findByUsername(newUser.username);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new RedirectView("/myprofile");
+        long id = user.id;
+        return "redirect:/users/"+ id;
+    }
+
+    @GetMapping("/users/{id}")
+    public String getDinosaurs(Principal p, Model m) {
+        System.out.println(p.getName());
+        m.addAttribute("principal", p);
+        return "userprofile";
     }
 
     @GetMapping("/login")
